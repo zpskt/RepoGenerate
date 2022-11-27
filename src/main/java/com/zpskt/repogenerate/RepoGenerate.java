edf55ada8d02be2da6b8c732f78c65b13dfbfbda
15866965614 <2233151897@qq.com>
2022-11-27 13:41:12 +0800

    初始化项目

---
+++ src/main/java/com/zpskt/repogenerate/RepoGenerate.java
@@ -0,0 +1,13 @@
+package com.zpskt.repogenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // springboot本尊
+public class RepoGenerate {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoGenerate.class, args);
+	}
+
+}
